f5af6387e28767485d6de2d089957df9fe9f9689
Saurabh Chavan <cvnsaurabh7@gmail.com>
2026-02-06 14:25:02 -0500

    google auth file modified

--- DeskApp/src/auth/GoogleAuth.js
+++ DeskApp/src/auth/GoogleAuth.js
@@ -24,7 +24,8 @@ const SCOPES = [
   'email',
   'profile',
   'https://www.googleapis.com/auth/calendar',
-  'https://www.googleapis.com/auth/gmail.readonly'
+  'https://www.googleapis.com/auth/gmail.readonly',
+  'https://www.googleapis.com/auth/gmail.compose'
 ].join(' ');
 
 /**
